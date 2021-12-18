c5e0545a635f7451834c30763a2b1320d9b9f146
Allen Kemp <wkemp@rewest.com>
2021-12-18 12:34:01 -0600

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { add_flush_callback } from 'svelte/internal';
+
+import * as FullDeckJSON from '../json/FullDeck.json';
+
+const fullDeck = FullDeckJSON.fullDeck;
+
+
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h1>Full Deck</h1>
+{#each fullDeck as {id, }}
